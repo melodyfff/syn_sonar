3885719c43bcbeb60aaafd7cb3a40d9a81fa5a4b
melodyfff <307208327@qq.com>
2018-08-25 00:28:49 +0800

    [syn_sonar] :coffee: init

---
+++ src/main/java/com/xinchen/syn_sonar/SynSonarApplication.java
@@ -0,0 +1,12 @@
+package com.xinchen.syn_sonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynSonarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynSonarApplication.class, args);
+    }
+}
